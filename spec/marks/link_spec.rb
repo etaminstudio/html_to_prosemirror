ba847bfadc8baf5f7d29b1ff04ebcde8a4a7c258
Hans Lemuet <hans@etamin.studio>
2026-02-18 19:53:44 +0100

    Fix nested marks & collapse spaces instead of removing them

--- spec/marks/link_spec.rb
+++ spec/marks/link_spec.rb
@@ -18,4 +18,37 @@ RSpec.describe HtmlToProsemirror::Marks::Link do
     renderer = HtmlToProsemirror::Renderer.new()
     expect(json).to eq renderer.render(html)
   end
+
+  it "preserves spaces between inline elements inside a link" do
+    html = '<p><a href="https://example.com"><em>Foo</em> and <em>Bar</em> </a>baz</p>'
+    link_mark = { type: 'link', attrs: { href: 'https://example.com' } }
+    json = {
+      type: 'doc',
+      content: [{
+        type: 'paragraph',
+        content: [{
+          type: 'text',
+          text: 'Foo',
+          marks: [link_mark, { type: 'italic' }]
+        }, {
+          type: 'text',
+          text: ' and ',
+          marks: [link_mark]
+        }, {
+          type: 'text',
+          text: 'Bar',
+          marks: [link_mark, { type: 'italic' }]
+        }, {
+          type: 'text',
+          text: ' ',
+          marks: [link_mark]
+        }, {
+          type: 'text',
+          text: 'baz'
+        }]
+      }]
+    }
+    renderer = HtmlToProsemirror::Renderer.new
+    expect(json).to eq renderer.render(html)
+  end
 end
